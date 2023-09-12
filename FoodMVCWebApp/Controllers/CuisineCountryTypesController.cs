8bf07f2bb3fc91a5a92e7ee651cb02b34fc3f6f3
aleksandrstarzhynskyi <a.stargik@gmail.com>
2023-09-12 17:44:10 +0300

    Added base controllers with views

---
+++ FoodMVCWebApp/Controllers/CuisineCountryTypesController.cs
@@ -0,0 +1,163 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FoodMVCWebApp.Data;
+using FoodMVCWebApp.Entities;
+
+namespace FoodMVCWebApp.Controllers
+{
+    public class CuisineCountryTypesController : Controller
+    {
+        private readonly FoodDbContext _context;
+
+        public CuisineCountryTypesController(FoodDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: CuisineCountryTypes
+        public async Task<IActionResult> Index()
+        {
+              return _context.CuisineCountryTypes != null ? 
+                          View(await _context.CuisineCountryTypes.ToListAsync()) :
+                          Problem("Entity set 'FoodDbContext.CuisineCountryTypes'  is null.");
+        }
+
+        // GET: CuisineCountryTypes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.CuisineCountryTypes == null)
+            {
+                return NotFound();
+            }
+
+            var cuisineCountryType = await _context.CuisineCountryTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cuisineCountryType == null)
+            {
+                return NotFound();
+            }
+
+            return View(cuisineCountryType);
+        }
+
+        // GET: CuisineCountryTypes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CuisineCountryTypes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] CuisineCountryType cuisineCountryType)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cuisineCountryType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cuisineCountryType);
+        }
+
+        // GET: CuisineCountryTypes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.CuisineCountryTypes == null)
+            {
+                return NotFound();
+            }
+
+            var cuisineCountryType = await _context.CuisineCountryTypes.FindAsync(id);
+            if (cuisineCountryType == null)
+            {
+                return NotFound();
+            }
+            return View(cuisineCountryType);
+        }
+
+        // POST: CuisineCountryTypes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] CuisineCountryType cuisineCountryType)
+        {
+            if (id != cuisineCountryType.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cuisineCountryType);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CuisineCountryTypeExists(cuisineCountryType.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cuisineCountryType);
+        }
+
+        // GET: CuisineCountryTypes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.CuisineCountryTypes == null)
+            {
+                return NotFound();
+            }
+
+            var cuisineCountryType = await _context.CuisineCountryTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cuisineCountryType == null)
+            {
+                return NotFound();
+            }
+
+            return View(cuisineCountryType);
+        }
+
+        // POST: CuisineCountryTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.CuisineCountryTypes == null)
+            {
+                return Problem("Entity set 'FoodDbContext.CuisineCountryTypes'  is null.");
+            }
+            var cuisineCountryType = await _context.CuisineCountryTypes.FindAsync(id);
+            if (cuisineCountryType != null)
+            {
+                _context.CuisineCountryTypes.Remove(cuisineCountryType);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CuisineCountryTypeExists(int id)
+        {
+          return (_context.CuisineCountryTypes?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ FoodMVCWebApp/Controllers/DishesController.cs
@@ -0,0 +1,174 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FoodMVCWebApp.Data;
+using FoodMVCWebApp.Entities;
+
+namespace FoodMVCWebApp.Controllers
+{
+    public class DishesController : Controller
+    {
+        private readonly FoodDbContext _context;
+
+        public DishesController(FoodDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Dishes
+        public async Task<IActionResult> Index()
+        {
+            var foodDbContext = _context.Dishes.Include(d => d.Category).Include(d => d.DifficultyLevel);
+            return View(await foodDbContext.ToListAsync());
+        }
+
+        // GET: Dishes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Dishes == null)
+            {
+                return NotFound();
+            }
+
+            var dish = await _context.Dishes
+                .Include(d => d.Category)
+                .Include(d => d.DifficultyLevel)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (dish == null)
+            {
+                return NotFound();
+            }
+
+            return View(dish);
+        }
+
+        // GET: Dishes/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Id");
+            ViewData["DifficultyLevelId"] = new SelectList(_context.DifficultyLevels, "Id", "Id");
+            return View();
+        }
+
+        // POST: Dishes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Recipe,Image,CategoryId,DifficultyLevelId,CuisineCountryTypelId")] Dish dish)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(dish);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Id", dish.CategoryId);
+            ViewData["DifficultyLevelId"] = new SelectList(_context.DifficultyLevels, "Id", "Id", dish.DifficultyLevelId);
+            return View(dish);
+        }
+
+        // GET: Dishes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Dishes == null)
+            {
+                return NotFound();
+            }
+
+            var dish = await _context.Dishes.FindAsync(id);
+            if (dish == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Id", dish.CategoryId);
+            ViewData["DifficultyLevelId"] = new SelectList(_context.DifficultyLevels, "Id", "Id", dish.DifficultyLevelId);
+            return View(dish);
+        }
+
+        // POST: Dishes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Recipe,Image,CategoryId,DifficultyLevelId,CuisineCountryTypelId")] Dish dish)
+        {
+            if (id != dish.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(dish);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DishExists(dish.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Id", dish.CategoryId);
+            ViewData["DifficultyLevelId"] = new SelectList(_context.DifficultyLevels, "Id", "Id", dish.DifficultyLevelId);
+            return View(dish);
+        }
+
+        // GET: Dishes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Dishes == null)
+            {
+                return NotFound();
+            }
+
+            var dish = await _context.Dishes
+                .Include(d => d.Category)
+                .Include(d => d.DifficultyLevel)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (dish == null)
+            {
+                return NotFound();
+            }
+
+            return View(dish);
+        }
+
+        // POST: Dishes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Dishes == null)
+            {
+                return Problem("Entity set 'FoodDbContext.Dishes'  is null.");
+            }
+            var dish = await _context.Dishes.FindAsync(id);
+            if (dish != null)
+            {
+                _context.Dishes.Remove(dish);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DishExists(int id)
+        {
+          return (_context.Dishes?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
