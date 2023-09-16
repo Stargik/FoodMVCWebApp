98ba22e0722528cfcc298d15bcb995b166601759
aleksandrstarzhynskyi <a.stargik@gmail.com>
2023-09-16 21:25:25 +0300

    Added tests

---
+++ FoodMVCWebAppTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using FoodMVCWebApp.Controllers;
+using FoodMVCWebApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodMVCWebAppTests;
+
+[TestClass]
+public class HomeControllerTests
+{
+    [TestMethod]
+    public void TestPrivacyView()
+    {
+        var controller = new HomeController();
+        var result = controller.Privacy() as ViewResult;
+        Assert.AreEqual("Privacy", result.ViewName);
+
+    }
+}
+
